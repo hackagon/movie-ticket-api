492f1e5e8bb161fbd37df68670c7be7a24f2d15c
hackagon <phonghiavan@gmail.com>
2020-05-04 20:14:06 +0700

    push

--- src/repositories/card.repository.ts
+++ src/repositories/card.repository.ts
@@ -1,16 +1,22 @@
-import {DefaultCrudRepository} from '@loopback/repository';
-import {Card, CardRelations} from '../models';
-import {StagingDataSource} from '../datasources';
 import {inject} from '@loopback/core';
+import {DefaultCrudRepository} from '@loopback/repository';
+import {StagingDataSource} from '../datasources';
+import {Card, CardRelations} from '../models';
 
 export class CardRepository extends DefaultCrudRepository<
   Card,
   typeof Card.prototype.id,
   CardRelations
-> {
+  > {
   constructor(
     @inject('datasources.staging') dataSource: StagingDataSource,
   ) {
     super(Card, dataSource);
   }
+}
+
+export enum CardLevel {
+  SILVER = "SILVER",
+  GOLD = "GOLD",
+  DIAMOND = "DIAMOND"
 }
